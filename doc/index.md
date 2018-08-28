3d4e8a3b72168cfd3bc6d6c5ab7402ad4ab13c3c
Tommy <16768208+baronTommy@users.noreply.github.com>
2018-08-28 20:20:48 +0900

    サービス

--- doc/index.md
+++ doc/index.md
@@ -9,7 +9,6 @@ ng generate component heroes
 ```bash
 # サービス作成
 
-# src/app 直下に ディレクトリが作成される
-# app.module.tsにも追記される
+# src/app 直下に ファイルが作成される
 ng generate service hero
 ```
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
+import {HeroService} from '../hero.service';
 
 // メタデータ
 @Component({
@@ -17,15 +17,22 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  heroes = HEROES;
+  heroes: Hero[];
 
-  constructor() {
+  constructor(private heroService: HeroService) {
   }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 }
+
